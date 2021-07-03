fcc89b40b1a8cd61455427126a26d4e2b29ed078
Rashmi1526 <rashmi.19b101039@abes.ac.in>
2021-07-03 16:56:57 +0530

    almost

--- harry.js
+++
@@ -1,7 +0,0 @@
-
-function available(){
-    document.getElementById('description').style.display='block';      
-}
-function remove(){
-    document.getElementById('description').style.display='none';      
-}
